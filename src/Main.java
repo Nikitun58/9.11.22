6178955143344ebb95121686ee6137eff33aa4e5
nikit <nikitaaa6@yandex.ru>
2023-01-20 01:58:29 +0800

    9.11.22

--- src/Main.java
+++ src/Main.java
@@ -16,10 +16,9 @@
         System.out.println("Задача №1");
         int[] arr = generateRandomArray();
         System.out.println(Arrays.toString(arr));
-        int sum = 0;
-        for (int i = 0; i < arr.length; i++) {
-                System.out.println("Сумма трат за месяц составила " + IntStream.of(arr).sum() + " рублей.");
-        }
+        for (int i = 0; i < arr.length; i++) ;
+        System.out.println("Сумма трат за месяц составила " + IntStream.of(arr).sum() + " рублей.");
+
         // Задача №2
         System.out.println("Задача №2");
         int min = arr[0];
